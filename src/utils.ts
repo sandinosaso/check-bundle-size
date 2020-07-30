7cf6992c79e8c8dba20b052f1f723f527fcaa426
sandinosaso <sandinosaso@gmail.com>
2020-07-30 02:10:01 -0400

    Let see now

--- src/utils.ts
+++ src/utils.ts
@@ -23,7 +23,11 @@ const prFiles = async (octokit: any, context: any): Promise<string[]> => {
     commit_sha: context.sha
   })
 
-  if (pr.data.length === 0) {
+  console.log(
+    'Got pr associated with this commit with context', context, pr
+  )
+
+  if (pr?.data?.length === 0) {
     throw new Error(`no PRs associated with commit ${context.sha}`)
   }
 
