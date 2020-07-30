29835acd324ddf4c50cffd9bd93065d73849b0e6
sandinosaso <sandinosaso@gmail.com>
2020-07-30 01:45:22 -0400

    Fix linter and typescript errors

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,31 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
-const context = github.context;
+import * as core from '@actions/core'
+import * as github from '@actions/github'
 
-import { prFiles, prPackages, sizeCheck, isMonorepo } from './utils';
+import {prFiles, prPackages, sizeCheck, isMonorepo} from './utils'
+const context = github.context
 
-const run = async () => {
-  const myToken = core.getInput('github_token');
+const run = async (): Promise<void> => {
+  const myToken = core.getInput('github_token')
 
-  const build_command: string = core.getInput('debug_command')
-  core.debug(`Build command ${build_command} ...`);
+  const debug_command: string = core.getInput('debug_command')
+  core.debug(`Build command ${debug_command} ...`)
 
   const octokit = github.getOctokit(myToken)
 
   try {
     if (isMonorepo()) {
-      const changedFiles = await prFiles(octokit, context);
-      const pkgs = prPackages(changedFiles);
+      const changedFiles = await prFiles(octokit, context)
+      const pkgs = prPackages(changedFiles)
 
-      await Promise.all(pkgs.map((pkg: any) => sizeCheck(core, octokit, context, pkg)));
+      await Promise.all(
+        pkgs.map(async (pkg: string) => sizeCheck(core, octokit, context, pkg))
+      )
     } else {
-      await sizeCheck(core, octokit, context, process.cwd());
+      await sizeCheck(core, octokit, context, process.cwd())
     }
   } catch (err) {
-    core.setFailed(err);
+    core.setFailed(err)
   }
 }
 
-run();
+run()
--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,13 @@
 /* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use strict'
-const path = require('path');
-const fs = require('fs-extra');
-const bytes = require('bytes');
-const { createGzip } = require('zlib');
-const artifact = require('@actions/artifact');
-const execa = require('execa');
-const globby = require('globby');
+import path from 'path'
+import fs from 'fs-extra'
+import bytes from 'bytes'
+import {createGzip} from 'zlib'
+import * as artifact from '@actions/artifact'
+import execa from 'execa'
+import globby from 'globby'
 
 /**
  * Get files for a PR
@@ -15,7 +16,7 @@ const globby = require('globby');
  * @param {Context} context Context object
  * @return {string[]} Returns the list of files names
  */
-const prFiles = async (octokit: any, context: any) => {
+const prFiles = async (octokit: any, context: any): Promise<string[]> => {
   const pr = await octokit.repos.listPullRequestsAssociatedWithCommit({
     owner: context.repo.owner,
     repo: context.repo.repo,
@@ -26,16 +27,16 @@ const prFiles = async (octokit: any, context: any) => {
     throw new Error(`no PRs associated with commit ${context.sha}`)
   }
 
-  const prFiles = await octokit.pulls.listFiles({
+  const pullRequestFiles = await octokit.pulls.listFiles({
     owner: context.repo.owner,
     repo: context.repo.repo,
     pull_number: pr.data[0].number
   })
 
-  return prFiles.data.map((f: any) => f.filename)
+  return pullRequestFiles.data.map((f: any) => f.filename)
 }
 
-const prPackages = (files: string[]) => {
+const prPackages = (files: string[]): string[] => {
   const baseDir = 'packages'
 
   const packages: string[] = []
@@ -52,12 +53,12 @@ const prPackages = (files: string[]) => {
   return packages
 }
 
-const gzipSize = (path: string): Promise<number> => {
+const gzipSize = async (filePath: string): Promise<number> => {
   return new Promise((resolve, reject) => {
     let size = 0
-    const pipe = fs.createReadStream(path).pipe(createGzip({ level: 9 }))
+    const pipe = fs.createReadStream(filePath).pipe(createGzip({level: 9}))
     pipe.on('error', reject)
-    pipe.on('data', (buf: Buffer) => {
+    pipe.on('data', (buf: any) => {
       size += buf.length
     })
     pipe.on('end', () => {
@@ -67,12 +68,15 @@ const gzipSize = (path: string): Promise<number> => {
 }
 
 const getBundleSizeDiff = async (): Promise<void> => {
-  const stats = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'dist/stats.json')))
+  const statsFileJson: string = fs.readFileSync(path.join(process.cwd(), 'dist/stats.json')).toString();
+  const stats = JSON.parse(statsFileJson);
   const gzip = await gzipSize(path.join(stats.outputPath, stats.assets[0].name))
-  const maxsize = 100; // bytes(config.bundlesize.maxSize)
-  const diff = gzip - maxsize;
+  const maxsize = 100 // bytes(config.bundlesize.maxSize)
+  const diff = gzip - maxsize
 
-  console.log('Use http://webpack.github.io/analyse/ to load "./dist/stats.json".')
+  console.log(
+    'Use http://webpack.github.io/analyse/ to load "./dist/stats.json".'
+  )
   // console.log(`Check previous sizes in https://bundlephobia.com/result?p=${pkg.name}@${pkg.version}`)
 
   if (diff > 0) {
@@ -89,11 +93,16 @@ const getBundleSizeDiff = async (): Promise<void> => {
  * @param {Context} context Context object
  * @param {string} baseDir base dir absolute dir
  */
-const sizeCheck = async (core: any, octokit: any, context: any, baseDir: string) => {
-  let check = null;
-  const buildCommand = core.getInput('command_for_building');
-  const pkgName = baseDir.split('/').pop();
-  const checkName = isMonorepo() ? `size: ${pkgName}` : 'size';
+const sizeCheck = async (
+  core: any,
+  octokit: any,
+  context: any,
+  baseDir: string
+): Promise<void> => {
+  let check = null
+  const buildCommand = core.getInput('command_for_building')
+  const pkgName = baseDir.split('/').pop()
+  const checkName = isMonorepo() ? `size: ${pkgName}` : 'size'
 
   try {
     check = await octokit.checks.create({
@@ -102,66 +111,59 @@ const sizeCheck = async (core: any, octokit: any, context: any, baseDir: string)
       name: checkName,
       head_sha: context.sha,
       status: 'in_progress'
-    });
+    })
 
     const out = await execa(buildCommand, ['-a', '-b'], {
       cwd: baseDir,
       localDir: '.',
       preferLocal: true,
-      env: { CI: true }
-    });
-    console.log('Size check for:', pkgName);
-    console.log(out.stdout);
+      env: { CI: "true" }
+    })
+    console.log('Size check for:', pkgName)
+    console.log(out.stdout)
 
-    await getBundleSizeDiff();
+    await getBundleSizeDiff()
 
-    const parts = out.stdout.split('\n');
-    const title = parts[2];
-    await octokit.checks.update(
-      {
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        check_run_id: check.data.id,
-        conclusion: 'success',
-        output: {
-          title: title,
-          summary: [parts[0], parts[1]].join('\n')
+    const parts = out.stdout.split('\n')
+    const title = parts[2]
+    await octokit.checks.update({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      check_run_id: check.data.id,
+      conclusion: 'success',
+      output: {
+        title,
+        summary: [parts[0], parts[1]].join('\n')
+      }
+    })
+
+    await artifact
+      .create()
+      .uploadArtifact(
+        `${pkgName}-size`,
+        await globby(['dist/*'], {cwd: baseDir, absolute: true}),
+        baseDir,
+        {
+          continueOnError: true
         }
-      }
-    );
-
-    await artifact.create().uploadArtifact(
-      `${pkgName}-size`,
-      await globby(['dist/*'], { cwd: baseDir, absolute: true }),
-      baseDir,
-      {
-        continueOnError: true
-      }
-    );
+      )
   } catch (err) {
-    await octokit.checks.update(
-      {
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        check_run_id: check.data.id,
-        conclusion: 'failure',
-        output: {
-          title: err.stderr ? err.stderr : 'Error',
-          summary: err.stdout ? err.stdout : err.message
-        }
+    await octokit.checks.update({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      check_run_id: check.data.id,
+      conclusion: 'failure',
+      output: {
+        title: err.stderr ? err.stderr : 'Error',
+        summary: err.stdout ? err.stdout : err.message
       }
-    );
-    throw err;
+    })
+    throw err
   }
 }
 
-const isMonorepo = () => {
-  return fs.existsSync(path.join(process.cwd(), 'packages'));
+const isMonorepo = (): boolean => {
+  return fs.existsSync(path.join(process.cwd(), 'packages'))
 }
 
-export {
-  prFiles,
-  prPackages,
-  sizeCheck,
-  isMonorepo
-}
+export {prFiles, prPackages, sizeCheck, isMonorepo}
